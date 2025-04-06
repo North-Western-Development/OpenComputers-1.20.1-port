e20b15c63b71cb33ca45dd6f39ff99ae8b956ce3
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2025-04-06 22:45:14 +0200

    Fix new scala warnings in filtering rule test

--- src/test/scala/InternetFilteringRuleTest.scala
+++ src/test/scala/InternetFilteringRuleTest.scala
@@ -8,13 +8,13 @@ import org.scalatestplus.junit.JUnitRunner
 import org.scalatestplus.mockito.MockitoSugar
 
 import java.net.InetAddress
-import scala.compat.Platform.EOL
 import scala.io.{Codec, Source}
 
 @RunWith(classOf[JUnitRunner])
 class InternetFilteringRuleTest extends AnyFunSpec with MockitoSugar {
   val config = autoClose(classOf[Settings].getResourceAsStream("/application.conf")) { in =>
-    val configStr = Source.fromInputStream(in)(Codec.UTF8).getLines().mkString("", EOL, EOL)
+    val eol = System.lineSeparator()
+    val configStr = Source.fromInputStream(in)(Codec.UTF8).getLines().mkString("", eol, eol)
     ConfigFactory.parseString(configStr)
   }
   val settings = new Settings(config.getConfig("opencomputers"))
@@ -69,12 +69,12 @@ class InternetFilteringRuleTest extends AnyFunSpec with MockitoSugar {
     !InternetCard.isRequestAllowed(settings, resolved, uriObj.getHost)
   }
 
-  def autoClose[A <: AutoCloseable, B](closeable: A)(fun: (A) ⇒ B): B = {
+  def autoClose[A <: AutoCloseable, B](closeable: A)(fun: (A) => B): B = {
     var t: Throwable = null
     try {
       fun(closeable)
     } catch {
-      case funT: Throwable ⇒
+      case funT: Throwable =>
         t = funT
         throw t
     } finally {
@@ -82,7 +82,7 @@ class InternetFilteringRuleTest extends AnyFunSpec with MockitoSugar {
         try {
           closeable.close()
         } catch {
-          case closeT: Throwable ⇒
+          case closeT: Throwable =>
             t.addSuppressed(closeT)
             throw t
         }
